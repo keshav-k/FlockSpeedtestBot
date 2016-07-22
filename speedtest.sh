43d7f63862b7799a2419780fdda2626191b65857
keshav2910 <keshav2910@users.noreply.github.com>
2016-07-22 20:16:43 +0530

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-result=`$1`
-result="{\"text\" : \"$result\"}"
+result="{\"text\" : \"`$1`\"}"
 echo "$result" > ~/speedtest.json
 curl -X POST $2 -H "Content-Type: application/json" --data @"$HOME/speedtest.json"
+rm ~/speedtest.json
